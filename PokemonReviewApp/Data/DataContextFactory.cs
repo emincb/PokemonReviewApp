98303c8b1e1027df4ce3b4095ade3bccd45af0f4
emincb <88873568+emincb@users.noreply.github.com>
2023-02-24 16:21:22 +0300

    Initial commit

---
+++ PokemonReviewApp/Data/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContextFactory :IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=pokemonreview;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ PokemonReviewApp/Migrations/20230223120402_seeddata.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Categories_CategoryId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Categories_CategoryId",
+                table: "PokemonCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Categories_CategoryId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Categories_CategoryId",
+                table: "PokemonCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategories_Pokemon_PokemonId",
+                table: "PokemonCategories",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Pokemon_PokemonId",
+                table: "PokemonOwners",
+                column: "PokemonId",
+                principalTable: "Pokemon",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
